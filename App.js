1fcf5b72b6c31f6125d1735325a604a251a0b243
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2020-08-13 18:06:15 +0200

    NativeApp

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import{View,Text,StyleSheet,ScrollView,Image,Button,Alert,Vibration,} from 'react-native';
+import Box from './component/Box';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <ScrollView style={style.container}>
+        <View>
+          <Text style={style.text}>hello</Text>
+          <Text style={style.textcenter}>hello</Text>
+          <Text style={style.textbold}>hello</Text>
+          <Image source = {require('./assets/konexio-logo_1.png')}/>
+          <Image 
+            source={{url:'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png'}}
+            style={{width:100,height:30}}
+          />
+          <Button
+            title='click'
+            onPress={()=>Alert.alert('simple ok')}
+          />
+          <Button
+            title='click'
+            onPress={()=>Vibration.vibrate(10)}
+          />
+          <Box/>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const style = StyleSheet.create({
+  container:{
+    paddingTop:70,
+    backgroundColor:'pink'
+  },
+  text:{
+    fontSize:30,
+  },
+  textcenter:{
+    marginTop:80,
+    marginBottom:80,
+    textAlign:'center',
+  },
+  textbold:{
+    marginTop:80,
+    marginBottom:80,
+    fontWeight:'bold'
   },
 });
+
+export default App;
+
+
